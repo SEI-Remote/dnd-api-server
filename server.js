73a47c1d895520f7580b208a0a74cb60c29070f0
Ben Manley <benjamintmanley@gmail.com>
2023-05-03 08:54:18 -0500

    add cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import 'dotenv/config.js'
 import express from 'express'
 import logger from 'morgan'
+import cors from 'cors'
 
 import './config/database.js'
 
@@ -9,6 +10,7 @@ import { router as monstersRouter } from './routes/monsters.js'
 
 const app = express()
 
+app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
